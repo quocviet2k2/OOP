65726edf70230476a5d4844b39a5fbac8336a29c
quocviet2k2 <quocviet16112k2@gmail.com>
2023-01-08 21:41:58 +0700

    xoa file

--- OOPPrinciples/oopprinciples.abstraction/Abstraction.java
+++
@@ -1,5 +0,0 @@
-package OOPPrinciples.oopprinciples.abstraction;
-public abstract class Abstraction {
-    public abstract double getAreaRectangle(int length ,int width);
-    public abstract double getAreaSquare(int edge);
-}
